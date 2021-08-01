51e6410ac365beb9e76ca415b20ac4d22ee5d015
kelzerock <kelzzzer@gmail.com>
2021-08-01 19:07:19 +0300

    add

--- first-progect/src/app/app.module.ts
+++ first-progect/src/app/app.module.ts
@@ -1,19 +1,18 @@
-import { PostComponent } from './post/post.component';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
-import { Post2Component } from './post2/post2.component';
-import { Post3Component } from './post3/post3.component';
 import { FormsModule } from '@angular/forms';
+import { PostFormComponent } from './post-form/post-form.component';
+import { PostComponent } from './post/post.component';
+
 
 
 
 @NgModule({
   declarations: [
     AppComponent,
+    PostFormComponent,
     PostComponent,
-    Post2Component,
-    Post3Component,
   ],
   imports: [
     BrowserModule,
